fb2386627aba0a53910bd56a78ee4361df02dad4
ViktorKOSZEGI <koszegiv.5@gmail.com>
2021-03-24 15:41:13 +0100

    Implement getCompetitorsNamesFromFile() method

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,35 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        Map<String, Integer> totalScoresPerCompetitor = new HashMap<>();
+        File file = new File(fileName);
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] data = line.split(",");
+                totalScoresPerCompetitor.put(data[0], calculateTotalScore(data));
+            }
+        } catch (Exception e) {
+            System.out.println("File not found!");
+        }
+        return totalScoresPerCompetitor.entrySet()
+                .stream()
+                .sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .map(entry -> entry.getKey())
+                .collect(Collectors.toList());
     }
 
+        private static int calculateTotalScore (String[] data) {
+        int sum = 0;
+        for (int i = 1; i < data.length; i++) {
+            sum += Integer.parseInt(data[i]);
+        }
+        return sum;
+    }
 }
